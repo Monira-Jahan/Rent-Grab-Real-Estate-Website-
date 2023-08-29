bbe945739e65b1dd32c55518de86574ed6b02c81
Monira Jahan <monirajahan459@gmail.com>
2023-08-29 23:19:52 +0600

    Final Commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { FaUserAlt } from "react-icons/fa";
 const Header = () => {
     return (
-        <div className="navbar bg-base-100 border shadow-xl rounded-xl mb-4">
+        <div className="navbar fixed z-10 bg-base-100 bg-opacity-80 border shadow-xl rounded-xl mb-4 mx-auto  max-w-screen-xl">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />Rent & Grab</svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-medium">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
                         <li><a>Home</a></li>
                         <li>
                             <a>Property</a>
@@ -23,7 +23,7 @@ const Header = () => {
 
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 font-medium">
+                <ul className="menu menu-horizontal px-1 font-semibold">
                     <li><a>Home</a></li>
                     <li><a>Property</a></li>
                     <li><a>Blog</a></li>
